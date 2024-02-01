28f669704fa5b8d1ed881cabb6dc1eabda6e703c
Pramod Gupta <pramodkumar.g@echidnainc.com>
2024-02-01 14:11:42 +0530

    created custom query resolver

---
+++ app/code/Echidna/GraphQl/Model/Resolver/BlogResolver.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Echidna\GraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class BlogResolver implements ResolverInterface
+{
+    public function resolve(
+        Field $field,
+              $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ): array
+    {
+        return [
+            'title' => 'My Awesome Blog',
+            'store' => $context->getExtensionAttributes()->getStore()->getName(),
+            'current_customer_id' => $context->getUserId(),
+            'user_type' => $context->getUserType(),
+        ];
+    }
+}
---
+++ app/code/Echidna/GraphQl/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Echidna_GraphQl', __DIR__);
